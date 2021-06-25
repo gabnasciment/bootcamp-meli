d0a481a4e035a319ea01af41b1ed383565006fda
gabnasciment <gabriel.anascimento@mercadolivre.com>
2021-06-25 13:28:32 -0300

    Exercicios do modulo 5

--- Ex_7.java
+++ Ex_7.java
@@ -8,8 +8,28 @@ public class Ex_7 {
         // evolução anual até que a empresa X ultrapasse a empresa Y em seu valor de empresa.
 
 
+        double valorEmpresaX = 1.13;
+        double percentualX = 1.48;
+
+        double valorEmpresaY = 18.4;
+        double percentualY = 0.32;
+
+        int ano = 2021;
+
+        System.out.println("Empresa X - 01/01"+ano+ " - Valor da empresa X: "+ valorEmpresaX);
+        System.out.println("Empresa Y - 01/01"+ano+ " - Valor da empresa Y: "+ valorEmpresaY);
 
 
+        while(valorEmpresaX <= valorEmpresaY){
+            valorEmpresaX = valorEmpresaX + (valorEmpresaX * percentualX);
+            valorEmpresaY = valorEmpresaY + (valorEmpresaY * percentualY);
+
+            ano+=1;
+
+        }
+        System.out.println();
+        System.out.println("Empresa X - " +ano+ " - Valor da empresa X: "+ valorEmpresaX);
+        System.out.println("Empresa Y - " +ano+ " - Valor da empresa Y: "+ valorEmpresaY);
 
     }
 }
